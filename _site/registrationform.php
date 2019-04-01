fe5a7be2ae0914bfd83ade1fb2b6ac3310d9f989
administrator@international-aset.com <>
2019-04-01 13:35:19 -0400

    made changes in registration form

--- _site/registrationform.php
+++ _site/registrationform.php
@@ -43,7 +43,7 @@ $message = 'Dear Colleague,
 
 Thank you for registering for ICBES 2019. If you have requested any official letters, please allow up to 5 business days to receive your documents.
 
-If you are an author, please make sure to send us your camera ready version and a signed copyright form via email to info@icbes.net. You can find the copyright form here: https://icbes.net/papers. Please note that failing to do so may result in an unsuccessful process of your registration.
+If you are an author, please make sure to send us your final version (.doc or .docx format) and a signed copyright form via email to info@icbes.net. You can find the copyright form here: https://icbes.net/papers. Please note that failing to do so may result in an unsuccessful process of your registration.
 
 You can find your registration details below. If there are any errors in the information you have provided, please write an email to us at registration@icbes.net mentioning the correct information. Please note that you SHOULD NOT refill the form.
 
